aeccc406f371d372e40442377dbc7dd8d4fdccef
gifta <152134013+stefani-gifta@users.noreply.github.com>
2025-03-11 17:26:25 +0000

    Modified all

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function _calculateScrollbarWidth() {
+  document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+}
+// recalculate on resize
+window.addEventListener('resize', _calculateScrollbarWidth, false);
+// recalculate on dom load
+document.addEventListener('DOMContentLoaded', _calculateScrollbarWidth, false); 
+// recalculate on load (assets loaded as well)
+window.addEventListener('load', _calculateScrollbarWidth);
+
+
 /* Music */
 var music = new Audio('../assets/Lukrembo - Kitchen (freetouse.com).mp3')
 // music.play();
