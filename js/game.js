bc4a11e06cabc304e72b4f26d9ef48ec4b419f76
gifta <152134013+stefani-gifta@users.noreply.github.com>
2025-03-25 05:29:23 +0000

    Edited background and game page

--- js/game.js
+++ js/game.js
@@ -2,5 +2,23 @@ document.getElementById("create-account-form").addEventListener("submit", functi
     event.preventDefault();
     document.getElementById("account-form").style.display = "none";
     document.getElementById("ingredient-selection").style.display = "block";
-    console.log("Hello world");
+    // console.log("Hello world");
 });
+
+const recipe = document.getElementsByClassName("recipe");
+for (let i = 0; i < recipe.length; i++) {
+    recipe[i].addEventListener("click", function () {
+        document.querySelectorAll(".recipe").forEach(item => {
+            item.style.transform = "scale(1)";
+            item.style.opacity = "0.7";
+        });
+        if(recipe[i].style.transform == "scale(1.2)") {
+            recipe[i].style.transform = "scale(1)";
+            recipe[i].style.opacity = "0.7";
+        } else {
+            recipe[i].style.transform = "scale(1.2)";
+            recipe[i].style.opacity = "1";
+            recipe[i].style.transition = "transform 0.3s ease";
+        }
+    });
+}
